b8d0e853b4e8ed2bb1c22c56af7e85fc598f3c2c
Kane Maslen <kanem@nookal.com>
2020-03-06 13:23:27 +1000

    Updates

---
+++ precache-manifest.b955aebd2257124a78593b2af20ef965.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a20e188e688bf22bc0de4d1b3bef746",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "60cc12c729fb046fc2fd",
+    "url": "/monsters-rolodex/static/css/main.70849a97.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60cc12c729fb046fc2fd",
+    "url": "/monsters-rolodex/static/js/main.ffebbbb2.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
